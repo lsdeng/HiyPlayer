2bf148175b0cdd87e2095208cbc8e8d78a4c4b6e
刘赛登 <liusd@tuya.com>
2020-06-06 14:58:31 +0800

    first commit

---
+++ app/src/main/java/com/hiy/hiyplayer/MainActivity.java
@@ -0,0 +1,23 @@
+package com.hiy.hiyplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ui.PlayerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    PlayerView mPlayerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SimpleExoPlayer player = new SimpleExoPlayer.Builder(this).build()
+
+        player.
+    }
+}
