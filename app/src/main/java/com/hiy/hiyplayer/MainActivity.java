11dc84a1547b2570c421569464b20099ca530e84
Shamao <994516742@qq.com>
2020-07-30 02:00:24 +0800

    日常提交

--- app/src/main/java/com/hiy/hiyplayer/MainActivity.java
+++ app/src/main/java/com/hiy/hiyplayer/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import io.flutter.app.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
 
 
     @Override
