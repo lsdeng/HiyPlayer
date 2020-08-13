c0727bced32744277c061785c019cb8230b55988
刘赛登 <liusd@tuya.com>
2020-08-14 00:08:24 +0800

    日常提交

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,5 +5,8 @@ extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_hiy_hiyplayer_page_SplashActivity_stringFromJNI(JNIEnv *env, jobject thiz) {
     std::string hello = "Hello from C++";
+    jclass jclass1 = env->GetObjectClass(thiz);
+//     调用 native 的函数 传入string 返回新的string
+
     return env->NewStringUTF(hello.c_str());
 }
